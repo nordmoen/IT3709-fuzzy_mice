d5b21b83847bbaa844b5f6fa05e410293c0e6f95
Jørgen Nordmoen <nordmoen90@gmail.com>
2012-10-30 13:28:51 +0100

    Added all the fuzzy sets we need. Still need to complete the eval method. We might also need some calculation for union and intersection for such sets, not quite sure yet

--- fuzzy_mice/fuzzy_sets.py
+++ fuzzy_mice/fuzzy_sets.py
@@ -18,7 +18,7 @@ class FuzzyTriangle(object):
         self.x1 = left[0]
         self.x2 = right[0]
         self.top_x = top[0]
-        #Create needed parts here to use later in the eval method
+        #TODO: Create needed parts here to use later in the eval method
 
     def eval(self, value):
         '''Calculate how much this value is within this triangle'''
@@ -27,6 +27,62 @@ class FuzzyTriangle(object):
         elif self.top_x < value <= self.x2:
             pass
         else:
-            '''Value is outside of this triangle so just return 0.0'''
+            #Value is outside of this triangle so just return 0.0
             return 0.0
 
+class FuzzyTrapez(objec):
+    '''A fuzzy set implementation which represents a trapeze in a two
+    dimensional graph'''
+    def __init__(self, left, top_left, top_right, right):
+        '''         top_left-----------top_right
+                    /                          \
+                   /                            \
+                left---------------------------right'''
+        self.x1 = left[0]
+        self.x2 = top_left[0]
+        self.x3 = top_right[0]
+        self.x4 = right[0]
+        #TODO Finish
+
+    def eval(self, value):
+        if self.x1 <= value <= self.x2:
+            pass
+        elif self.x2 < value <= self.x3:
+            pass
+        elif self.x3 < value <= self.x4:
+            pass
+        else:
+            return 0.0
+
+class FuzzyGradient(object):
+    '''Fuzzy set implementation representing a gradient, everything inside the
+    gradient is inside the fuzzy set'''
+    def __init__(self, left, right):
+        '''If reverse is true we have a line like:
+             /
+            / All this is within the set
+        else:
+                              \
+        All this is within     \'''
+        self.reverse = left[1] < right[1]
+        self.x1 = left[0]
+        self.x2 = right[0]
+        self.top = float(left[1]) if not self.reverse else float(right[1])
+
+    def eval(self, value):
+        if not self.reverse:
+            if value <= self.x1:
+                return self.top
+            elif self.x1 < value <= self.x2:
+                #TODO fix calculation
+                pass
+            else:
+                return 0.0
+        else:
+            if value >= self.x2:
+                return self.top
+            elif self.x1 <= value < self.x2:
+                #TODO fix calculation
+                pass
+            else:
+                return 0.0
